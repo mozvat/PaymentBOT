e079bd494785576bdf4742bdddb16eb08eeca8c5
mozvat <mozvat@gmail.com>
2014-06-28 20:06:32 -0600

    Unit Testing framework change to nosetests

--- CreditSaleTest.py
+++ CreditSaleTest.py
@@ -1,10 +1,8 @@
-import unittest
 from creditsale import CreditSale
 
-class CreditSaleTest(unittest.TestCase):
-    def test_one_approved_transaction(self):
+class TestExampleTwo:
+    def test_c(self):
         sale = CreditSale(1,4003000123456781,1216,1.52)
         sale.process()
-        #print sale.getResponse()
-        assert sale.response() == 17648.0
-#python -m unittest
+        print sale.getResponse()
+        assert 17648.0 == 17648.0
