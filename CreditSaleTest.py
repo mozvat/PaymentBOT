39e84c31f8e4c5609b27c39414ddd211820757f6
mozvat <mozvat@gmail.com>
2014-06-29 13:20:39 -0600

    NanoTests admin testing

--- CreditSaleTest.py
+++ CreditSaleTest.py
@@ -1,8 +1,8 @@
 from creditsale import CreditSale
 
-class TestExampleTwo:
-    def test_c(self):
+class Test_CreditSale:
+    def test_successful_creditsale(self):
         sale = CreditSale(1,4003000123456781,1216,1.52)
-        sale.process()
-        print sale.getResponse()
-        assert 17648 == 17648
+        response = sale.process()
+        print response
+        assert 17648 != 17648
