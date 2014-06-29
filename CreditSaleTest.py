32014001925312f9f9e7d0a9173c4c0a05355ce0
mozvat <mozvat@gmail.com>
2014-06-29 14:09:18 -0600

    First real production test added. This test checks for a successful transaction

--- CreditSaleTest.py
+++ CreditSaleTest.py
@@ -1,8 +1,11 @@
 from creditsale import CreditSale
+import json
 
 class Test_CreditSale:
     def test_successful_creditsale(self):
         sale = CreditSale(1,4003000123456781,1216,1.52)
         response = sale.process()
-        print response
-        assert 17648 != 17648
+        #print response
+        data = response.json()
+        assert data['CmdStatus'] == "Approved"
+
