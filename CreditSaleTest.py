f675385ab8d16450ad2dd2e9aa2189c90754ea13
mozvat <mozvat@gmail.com>
2014-06-28 18:05:19 -0600

    Added Assert test

--- CreditSaleTest.py
+++ CreditSaleTest.py
@@ -5,6 +5,6 @@ class CreditSaleTest(unittest.TestCase):
     def test_one_approved_transaction(self):
         sale = CreditSale(1,4003000123456781,1216,1.52)
         sale.process()
-        print sale.getResponse()
-       # assert sale.response() == 17648.0
+        #print sale.getResponse()
+        assert sale.response() == 17648.0
 #python -m unittest
