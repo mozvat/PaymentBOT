31f3ed0ef410bb0c35a2f5c041ef6c21a430ec56
mozvat <mozvat@gmail.com>
2014-06-29 22:39:27 -0600

    Adding Python package project setupscript

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='RaspberryPI',
+      version='1.0',
+      description='RaspberryPI Payments Engine',
+      author='Matt Ozvat',
+      author_email='mozvat@gmail.com',
+      url='http://www.github.com/mozvat/raspberrypi',
+      packages=['distutils', 'distutils.command'],
+      )
