ced29a40eb6e30ba6790e8eca8e4eb62191692fa
mozvat <mozvat@gmail.com>
2014-06-30 22:56:55 -0600

    Added repeat script testing gift and credit sales

--- repeatransaction.sh
+++ repeatransaction.sh
@@ -1,6 +1,25 @@
 while true
+	clear
 	do
-		echo "Gift-Transaction"
-		python giftsale.py
-		sleep 6
+		echo "----------------------------------------------"
+		echo "Pulling latest RaspberryPI Version from GitHub" 
+		echo "Accessing GitHub/mozvat/RaspberryPI"
+		sleep 1		
+		echo "."
+		sleep 1
+		echo "."
+		sleep 1
+		git pull
+		echo "---------------------------"
+		echo "Executing: Gift-Transaction"
+		echo "Test file: test_giftsale.py"		
+		sleep 1
+		echo "Processing..."		
+		nosetests test_giftsale.py
+		echo "---------------------------"
+		echo "Executing: Credit-Transaction"
+		echo "Test file: test_creditsale.py"
+		sleep 1
+		echo "Processing..."
+		nosetests test_creditsale.py
 	done
