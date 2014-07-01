2b6fb7fffc4e47f7c3b5c7b12bd90770d12f1b42
mozvat <mozvat@gmail.com>
2014-06-30 18:57:04 -0600

    changed constructor, added private member variables, created commented selftests

--- giftsale.py
+++ giftsale.py
@@ -1,28 +1,65 @@
-print "Sending a Prepaid transaction to Mercury's certification network..."
+'''
+    Author: mozvat
+    
+    The GiftSale class is responsible for accepting the minimum required arguments to request
+    a payment authorization via a HTTP POST request to Mercury's
+    web services.  It will return the raw response in JSON format.
+    
+    #Specificity guidelines:
+    
+    This class:
+    - will NOT format response data from Web Services. It will allow the consumer to format the
+    response.
+    - However, this class WILL format input data and transform to the approrpriate request format.
+    - will NOT expose any public attributes. This class is designed with a command pattern in mind and
+    not deisned to hold stateful data. 
+    '''
 
 import json
 import lib.requests.api as API
 from lib.requests.auth import HTTPBasicAuth
 
 class GiftSale(object):
-    def __init__(self, InvoiceNo):
+    def __init__(self, InvoiceNo, AcctNo, ExpDate, Amount):
         self.invoiceNo = InvoiceNo
+        self.accountNo = AcctNo
+        self.expDate = ExpDate
+        self.purchase = Amount
 
     def process(self):
         # TranType: {Credit,PrePaid, PayPal, etc} / TranCode {Sale, Return, Void}'
         url = 'https://w1.mercurycert.net/PaymentsAPI/Prepaid/Sale'
 
-        payload = {"InvoiceNo":"291",
-                    "RefNo":"214",
-                    "AcctNo":"7712950000000000316",
-                    "ExpDate":"1215",
-                    "Purchase":"1.00"}
+        payload = {'InvoiceNo': self.invoiceNo,
+                    'RefNo': self.invoiceNo,
+                    'Memo': 'Ozvats POS',
+                    'AcctNo': self.accountNo,
+                    'ExpDate': self.expDate,
+                    'Purchase': self.purchase
+                    }
             
-        r = API.post(url, data=json.dumps(payload), auth=('112438931977591', 'xyz'))
+        response = API.post(url, data=json.dumps(payload), auth=('112438931977591', 'xyz'))
     
-        print r.text
+        return response
 
 
 
-myGiftSale = GiftSale(12)
-myGiftSale.process()
+
+
+'''
+    ------------------------
+    ---Unit test script ----
+    ------------------------
+    
+    
+    Use below code for quick check testing,
+    otherwise leverage "nosetests"
+    
+    
+sale = GiftSale(11git 33,7712950000000000316,1216,1.57)
+response = sale.process()
+print response
+data = response.json()
+print data['CmdStatus']
+
+'''
