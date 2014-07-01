3263ec742d9828afe842eb8014fdbb12fec71e18
mozvat <mozvat@gmail.com>
2014-06-30 19:57:59 -0600

    Adding test_gift

---
+++ test_giftsale.py
@@ -0,0 +1,17 @@
+from giftsale import GiftSale
+import json
+
+class Test_GiftSale:
+    def test_successful_giftsale(self):
+        try:
+            sale = GiftSale(123,7712950000000000316,1216,1.57)
+            response = sale.process()
+            data = response.json()
+            print("-------------------------------")
+            print("Testing: AcctNo == 771295XXXXXXXXX0316")
+            print("-------------------------------")
+            assert data["AcctNo"] == "771295XXXXXXXXX0316"
+        except AssertionError as err:
+            print("RESULTS: AcctNo was not equal to the value 771295XXXXXXXXX0316")
+            print(err)
+            raise
