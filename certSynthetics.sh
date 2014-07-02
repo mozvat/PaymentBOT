f3cbbe3d59b24e8fc77e8efe8a9cfae3abdba6f7
mozvat <mozvat@gmail.com>
2014-07-02 16:02:19 -0600

    addeing cert synthetic

---
+++ certSynthetics.sh
@@ -0,0 +1,24 @@
+while true
+    do
+        echo "----------------------------------------------"
+		echo "Pulling latest RaspberryPI Version from GitHub" 
+		echo "Accessing GitHub/mozvat/RaspberryPI"
+		sleep 1		
+		echo "."
+		sleep 1
+		echo "."
+		sleep 1
+		echo "---------------------------"
+		echo "Executing: Gift-Transaction"
+		echo "Test file: test_giftsale.py"		
+		sleep 1
+		echo "Processing..."		
+		nosetests test_giftsale.py
+		echo "---------------------------"
+		echo "Executing: Credit-Transaction"
+ 		echo "Test file: test_creditsale.py"
+ 		sleep 1
+		echo "Processing..."
+        nosetests test_creditsale.py
+        sleep 300
+  	done
