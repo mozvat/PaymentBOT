b9a2c215395d9e8273d1dabd95d1341d91ed1592
mozvat <mozvat@gmail.com>
2014-07-01 23:04:44 -0600

    Adding code to accept swipe

--- main.py
+++ main.py
@@ -2,26 +2,46 @@ from giftsale import GiftSale
 from creditsale import CreditSale
 import sys
 
-print "---------------------------------"    
-print "Welcome to the Python POS system!"
-print "---------------------------------\n\n"
+print "--------------------------------------"
+print "Welcome to the RaspberryPI POS system!"
+print "--------------------------------------\n\n"
 print "---------------------------------"
 print "-            MENU              -"
 print "---------------------------------"
-print "[1] - Credit Sale"
-print "[2] - Gift Sale"
+print "[1] - Start listening"
+print "[2] - Congifuration"
+print "[3] - Status"
 print "---------------------------------"
 
-selection = input("Select Transaction type: ")
+selection = input("Selection: ")
 
 if selection == 1:
-    myCreditSale = CreditSale(12)
-    myCreditSale.process()
+    #Wait for Swipe:
+
+    trackData = "%B5499000090006781^TEST/MPS^15120000000000000?;5499000090006781=15120000000000000000?|0600|74F1C98C2EF08240B92D66C96F17EA217155BD168C6A394B0B318E834CDE6D07B5F00FC8C9C02ED4486E95681D169B2D|---2B0FF19B93EAE51780787276264A029276A3BDCA2622A3FD12E3F6A5714B885984F2096B7E9FE1A6---||61401000|D2CA4A6339A00C08E7A8DCDD80447BFFA46F24F3386AD7B46EDD44622B3E28FDCC376907B3555C67B826AF8A21C5851A1F5A7AD01DFAE799|B06349B042812AA|BDAAB3EA6505F532|---9012090B06349B00006E---|B973||1000"
+
+#trackData = raw_input("Waiting for encrypted track data...\n\n")
+#   print "\n\nNext, we need to parse the track data, look for delimenters\n\n"
+    #Parse the track data into the appropriate paramaters to invoke a Credit sale.
+
+
+    myString="Hello there !bob@"
+    mySubString=trackData[trackData.find("%")+1:trackData.find("?")]
+    s = "Name1=Value1;Name2=Value2;Name3=Value3"
+    d = dict(item.split("=") for item in s.split(";"))
+    print "second" + d[2]
+    print d
+
+
+
 else:
-    myGiftSale = GiftSale(12)
-    myGiftSale.process()
+    print "Hello"
+#myCreditSale = CreditSale(1234, AcctNo, ExpDate, 1.46)
+#myCreditSale.process()
+
+
 
 def main(argv=None):
     if argv is None:
         argv = sys.argv
-    print "Script Name: " + sys.argv[0]
+    print "Arg Name: " + sys.argv[0]
