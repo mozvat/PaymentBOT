68346202b111ec765c05a9b6612dc06d682ed57a
mozvat <mozvat@gmail.com>
2014-07-03 15:58:21 -0600

    Added While loop for main menu of Raspberry POS

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
-from giftsale import GiftSale
-from creditsale import CreditSale
 from encryptedcreditsale import EncryptedCreditSale
 import sys
 
 print "--------------------------------------"
 print "Welcome to the RaspberryPI POS system!"
 print "--------------------------------------\n\n"
-print "---------------------------------"
-print "-            MENU              -"
-print "---------------------------------"
-print "[1] - Start listening"
-print "[2] - Congifuration"
-print "[3] - Status"
-print "---------------------------------"
 
-selection = input("Selection: ")
 
-if selection == 1:
-    #Wait for Swipe:
-    trackData = raw_input("\n\nSwipe...\n\n")
 
-    containsEncryptedBlock, containsEncryptedKey, genericData = trackData.split("||")
-    print containsEncryptedKey
-    generic1, generic2, generic3, encryptedBlock = containsEncryptedBlock.split("|")
+selection = 0
 
-    generic4, generic5, generic6, generic7, encryptedKey, generic8 = containsEncryptedKey.split("|")
+while (selection != 4):
 
-    print encryptedKey
-    print encryptedBlock
+    print "---------------------------------"
+    print "-            MENU              -"
+    print "---------------------------------"
+    print "[1] - Start listening"
+    print "[2] - Congifuration"
+    print "[3] - Status"
+    print "[4] - Quit"
+    print "---------------------------------"
 
-    myEncryptedCreditSale = EncryptedCreditSale(122,1.00 ,encryptedBlock, encryptedKey)
-    response = myEncryptedCreditSale.process()
-    print response
-    data = response.json()
-    print data["TextResponse"]
+    selection = input("Selection: ")
 
-#else:
-#    print "Hello"
-#myCreditSale = CreditSale(1234, AcctNo, ExpDate, 1.46)
-#myCreditSale.process()
+    if selection == 1:
+        #Wait for Swipe:
+        trackData = raw_input("\n\nSwipe...\n\n")
+
+        containsEncryptedBlock, containsEncryptedKey, genericData = trackData.split("||")
+        print containsEncryptedKey
+        generic1, generic2, generic3, encryptedBlock = containsEncryptedBlock.split("|")
+
+        generic4, generic5, generic6, generic7, encryptedKey, generic8 = containsEncryptedKey.split("|")
+
+        myEncryptedCreditSale = EncryptedCreditSale(122, encryptedBlock, encryptedKey, 1.25)
+        response = myEncryptedCreditSale.process()
+        print response
+        data = response.json()
+        print data["TextResponse"]
+
+    if  selection == 2:
+        print "Congifigure"
+
+    if  selection == 3:
+        print "Status"
+
+    else:
+        print "Exiting System"
 
 
 
