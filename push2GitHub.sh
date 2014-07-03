e7f2dae91e12d378e6417bdafcbd22b79943f4b8
mozvat <mozvat@gmail.com>
2014-07-02 22:14:48 -0600

    Shell script pushing local commits to GitHub to fire off CI

---
+++ push2GitHub.sh
@@ -0,0 +1,17 @@
+clear
+while true
+	do
+		echo "Git Status: :" 
+		git status 
+		echo "----------------------------------------------"
+		echo "Pushing latest RaspberryPI Version from GitHub" 
+		echo "GitHub/mozvat/RaspberryPI"
+		sleep 1		
+		echo "."
+		sleep 1
+		echo "."
+		sleep 1
+		git push
+		echo "---------------------------"
+		sleep 300
+	done
