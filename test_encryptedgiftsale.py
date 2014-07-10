256d1b4b0d0f813d63ba77a21809b450fca64a9b
mozvat <mozvat@gmail.com>
2014-07-10 08:36:52 -0600

    Added Comment for debugging in the case of an assert error

--- test_encryptedgiftsale.py
+++ test_encryptedgiftsale.py
@@ -4,7 +4,7 @@ import json
 class Test_EncryptedGiftSale:
     def test_successful_encryptedgiftsale(self):
         try:
-            myEncryptedGiftSale = EncryptedGiftSale(122,1.00,'C8C8F9536826D5450E734953206E7F4DC6812C6858037F5ABF23D9F83F948AF7','9012090B06349B000056')
+            myEncryptedGiftSale = EncryptedGiftSale(122,1.00,"C8C8F9536826D5450E734953206E7F4DC6812C6858037F5ABF23D9F83F948AF7","9012090B06349B000056")
             response = myEncryptedGiftSale.process()
             print response
             data = response.json()
@@ -15,5 +15,6 @@ class Test_EncryptedGiftSale:
             assert data["AcctNo"] == "605011XXXXXXXXX0146"
         except AssertionError as err:
             print("AcctNo was not equal to the value '605011XXXXXXXXX0146'")
+            print("Adding additional text")
             print(err)
             raise
