e2e92394e1c2c809594345ba54b09e491dc42549
mozvat <mozvat@gmail.com>
2014-07-16 17:11:42 -0600

    Personafied Rigel

--- push2GitHub.sh
+++ push2GitHub.sh
@@ -1,17 +1,24 @@
 clear
+say "I am Rigel"
 while true
 	do
-		echo "Git Status: :" 
-		git status 
-		echo "----------------------------------------------"
-		echo "Pushing latest RaspberryPI Version from GitHub" 
-		echo "GitHub/mozvat/RaspberryPI"
-		sleep 1		
-		echo "."
-		sleep 1
-		echo "."
-		sleep 1
+		echo "@Rigel: I am invoking Git status"
+		sleep 3
+		echo 
+		echo "---------------Git Status Results--------------------"
+		git status
+		echo
+		sleep 3
+		echo "@Rigel: I am invoking Git push"
+		sleep 3
+		echo
+		echo "---------------Git Push Results--------------------"
 		git push
-		echo "---------------------------"
+		echo	
+		sleep 3
+		echo "______________________________________"
+		echo "@Rigel: I am hibernating for 5 minutes"
+		echo "______________________________________"
 		sleep 300
 	done
+say "Rigel is sleeping"
