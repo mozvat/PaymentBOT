2e0c07ae2328187ce7fa57f1a9a0a35b1d5b0bf6
mozvat <mozvat@gmail.com>
2014-07-16 22:28:50 -0600

    updated Rigel script

--- push2GitHub.sh
+++ push2GitHub.sh
@@ -2,23 +2,26 @@ clear
 say "I am Rigel"
 while true
 	do
-		echo "@Rigel: I am invoking Git status"
+		clear
+		echo "@Rigel: I am invoking the git status command"
 		sleep 3
 		echo 
 		echo "---------------Git Status Results--------------------"
 		git status
 		echo
 		sleep 3
-		echo "@Rigel: I am invoking Git push"
+		echo "@Rigel: I am invoking the git push command"
 		sleep 3
 		echo
 		echo "---------------Git Push Results--------------------"
 		git push
 		echo	
 		sleep 3
-		echo "______________________________________"
-		echo "@Rigel: I am hibernating for 5 minutes"
-		echo "______________________________________"
+		echo '.'
+		sleep 1
+		echo '.'
+		sleep 1
+		echo "@Rigel: I am hibernating for 5 minutes..."
 		sleep 300
 	done
 say "Rigel is sleeping"
